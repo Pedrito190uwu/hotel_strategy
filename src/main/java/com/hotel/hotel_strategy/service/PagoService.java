19e7f49d0e77ab0c96fdcd2b2c0a336d7ad2e096
Pedrito190uwu <mariagaribello190@gmail.com>
2026-06-28 22:14:44 -0500

    feat: Implementar contexto del patron Strategy

--- src/main/java/com/hotel/hotel_strategy/service/PagoService.java
+++ src/main/java/com/hotel/hotel_strategy/service/PagoService.java
@@ -1,5 +1,16 @@
 package com.hotel.hotel_strategy.service;
 
+import com.hotel.hotel_strategy.strategy.MetodoPago;
+
 public class PagoService {
-    
+
+    private MetodoPago metodoPago;
+
+    public void setMetodoPago(MetodoPago metodoPago) {
+        this.metodoPago = metodoPago;
+    }
+
+    public String realizarPago(double monto) {
+        return metodoPago.pagar(monto);
+    }
 }

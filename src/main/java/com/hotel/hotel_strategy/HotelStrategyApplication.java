c26045418961116abfb5bef8c76c241d4908af1a
Pedrito190uwu <mariagaribello190@gmail.com>
2026-06-28 21:53:27 -0500

    feat:inicio de proyecto hotel strategy

---
+++ src/main/java/com/hotel/hotel_strategy/HotelStrategyApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.hotel_strategy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelStrategyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelStrategyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hotel/hotel_strategy/HotelStrategyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hotel.hotel_strategy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelStrategyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
